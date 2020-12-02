1b8bb1123fb82fa197efcd3320b4907f60d920a6
gkulik-6581 <66838666+gkulik-6581@users.noreply.github.com>
2020-12-02 14:30:48 -0500

    Created variables

---
+++ GuessTheNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GuessTheNumber
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("I am thinking of a whole number between 1 and 50. Enter your guess.");
+            string userGuess = Console.ReadLine();
+            Random rnd = new Random();
+            int compNum = rnd.Next(1, 50);
+            int guessToInt = Int32.Parse(userGuess);
+        }
+    }
+}
