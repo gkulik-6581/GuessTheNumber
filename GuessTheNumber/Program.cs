6f6f4f1b07c3e3fb3af266f8cbb6f194cff1b87c
gkulik-6581 <66838666+gkulik-6581@users.noreply.github.com>
2020-12-14 14:02:46 -0500

    Finished for now
    I might add hints for incorrect guesses

--- GuessTheNumber/Program.cs
+++ GuessTheNumber/Program.cs
@@ -10,7 +10,51 @@ namespace GuessTheNumber
             string userGuess = Console.ReadLine();
             Random rnd = new Random();
             int compNum = rnd.Next(1, 50);
+<<<<<<< Updated upstream
             int guessToInt = Int32.Parse(userGuess);
+=======
+            int guessToInt;
+            int numOfTries = 0;
+            string cont;
+            bool correct = false;
+
+            //TODO possibly add hints for higher or lower?
+
+            //Console.WriteLine($"Answer " + compNum);
+
+            while (correct == false)
+            {
+                numOfTries++;
+                userGuess = Console.ReadLine();
+                guessToInt = Int32.Parse(userGuess);
+
+                if (guessToInt == compNum)
+                {
+                    numOfTries++;
+                    correct = true;
+                    Console.WriteLine($"Your guess is correct! The number was " + compNum + ". Congratulations. It only took you " + numOfTries + " guess(es).");
+                }
+
+                else if (guessToInt < 1 || guessToInt > 50)
+                {
+                    Console.WriteLine("Incorrect input. Enter a whole number between 1 and 50. Try again.");
+                    continue;
+                }
+
+                else if (guessToInt != compNum)
+                {
+                    Console.WriteLine("Your guess is incorrect. Would you like to try again? Y or N?");
+                    cont = Console.ReadLine();
+                    cont = cont.ToUpper();
+
+                    if (cont != "Y")
+                    {
+                        break;
+                    }
+                    Console.WriteLine("Enter your next guess.");
+                }
+            }
+>>>>>>> Stashed changes
         }
     }
 }
